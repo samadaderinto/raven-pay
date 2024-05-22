e69ae4f4657ddc5f6f83b00e404fa0c2445d1a9e
samad aderinto <adesamad1234@gmail.com>
2024-05-22 02:59:44 +0100

    setup complete
    off to styling

--- src/layouts/header/header.tsx
+++ src/layouts/header/header.tsx
@@ -1,6 +1,19 @@
+import { GlobeIcon, LogoIcon, MenuIcon } from "../../assets/icons";
+import ProfileImage from "../../assets/profile.png";
+
+
 export const Header = () => {
   return (
-    <div>header</div>
+    <div className="header">
+      <LogoIcon/>
+
+      <div className="">
+        <img src={ProfileImage} alt="user profile"/>
+        <GlobeIcon/>
+        <MenuIcon />
+      </div>
+
+    </div>
   )
 }
 
