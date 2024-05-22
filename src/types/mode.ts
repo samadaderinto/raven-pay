43e62620c1ad40ca39f627e557cc1c3211ace09d
samad aderinto <adesamad1234@gmail.com>
2024-05-22 10:19:49 +0100

    Update mode.ts

--- src/types/mode.ts
+++ src/types/mode.ts
@@ -2,6 +2,6 @@ import React from "react";
 
 export type ModeProps = {
     mode: string;
-    setMode: React.Dispatch<React.SetStateAction<string>>;
+    setMode?: React.Dispatch<React.SetStateAction<string>>;
    
 };
