9c6a5930e364f51499bd5be4518720a037cbcf78
samad aderinto <adesamad1234@gmail.com>
2024-05-22 13:56:21 +0100

    Update order-history.tsx

--- src/components/order-history/order-history.tsx
+++ src/components/order-history/order-history.tsx
@@ -2,7 +2,12 @@ import "./order-history.css"
 
 
 export const OrderHistory = () => {
-  return (
-    <div>OrderHistory</div>
-  )
+    return (
+        <div>
+            <h3>No Open Orders</h3>
+            <div className="">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id pulvinar nullam sit imperdiet pulvinar.
+            </div>
+        </div>
+    )
 }
