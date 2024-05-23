b097c0f16afa06f9bf1adbdb508207c74589a84a
samad aderinto <adesamad1234@gmail.com>
2024-05-23 03:35:39 +0100

    chart.js

--- src/components/chart/chart.tsx
+++ src/components/chart/chart.tsx
@@ -5,7 +5,7 @@ import "./chart.css";
 export const Chart = () => {
 
   return (
-    <div id="scichart-root" style={{ width: '100%', height: '100%' }}>
+    <div id="" style={{ width: '100%', height: '100%' }}>
 pppp
     </div>
   )
